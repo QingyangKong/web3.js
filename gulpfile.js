91cd298bd0d52dd683a1f422b9955cbd9ece2c3f
duanyytop <duanyytop@gmail.com>
2018-02-08 15:52:37 +0800

    support browser and update npm version to 0.5.4

--- gulpfile.js
+++ gulpfile.js
@@ -49,7 +49,7 @@ gulp.task('bower', ['version'], function(cb){
 });
 
 gulp.task('lint', [], function(){
-    return gulp.src(['./*.js', './lib/*.js', './cita/*.js'])
+    return gulp.src(['./*.js', './lib/*.js'])
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
@@ -89,7 +89,8 @@ gulp.task('standalone', ['clean'], function () {
 });
 
 gulp.task('watch', function() {
-    gulp.watch(['./lib/*.js', './cita/*.js'], ['lint', 'build']);
+    gulp.watch(['./lib/*.js'], ['lint', 'build']);
 });
 
 gulp.task('default', ['version', 'lint', 'clean', 'light', 'standalone']);
+
