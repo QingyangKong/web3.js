fc33d4dbc6beec1ea12869b213ccf0fdd1aa9902
duanyytop <duanyytop@gmail.com>
2018-05-17 17:46:59 +0800

    update version 0.5.9

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 /* jshint ignore:start */
 Package.describe({
   name: 'cita:web3',
-  version: '0.5.8',
+  version: '0.5.9',
   summary: 'CITA JavaScript API, middleware to talk to a CITA node over RPC',
   git: 'https://github.com/ethereum/ethereum.js',
   // By default, Meteor will default to using README.md for documentation.
